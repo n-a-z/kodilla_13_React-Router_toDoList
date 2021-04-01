b7db73e90bf42a2fdadaf9fe799d380d3eee8ff3
Piotr Stepien <naz@o2.pl>
2021-04-01 15:58:12 +0200

    Add List.addColumn

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -22,6 +22,22 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   };
 
+  addColumn(title) {
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
   render() {
     return (
       <section className={styles.component}>
@@ -35,10 +51,10 @@ class List extends React.Component {
           ))}
         </div>
         <div className={styles.creator}>
-            <Creator
-              text={settings.columnCreatorText}
-              action={title => this.addColumn(title)}
-            />
+          <Creator
+            text={settings.columnCreatorText}
+            action={title => this.addColumn(title)}
+          />
         </div>
       </section>
     );
