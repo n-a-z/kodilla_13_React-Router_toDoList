1d112f0273bbb6c58a182e9342ab18c2b79ce4e5
Piotr Stepien <naz@o2.pl>
2021-04-01 09:06:04 +0200

    Add props title

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,12 @@
-import React from "react";
-import styles from "./List.scss";
-import Hero from "../Hero/Hero.js";
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} />
       </section>
     );
   }
