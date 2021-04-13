a7c2a76d8cd04efaf8ec96e8cac742558b9cd48d
naz <naz@o2.pl>
2021-04-13 08:50:13 +0200

    Add Button PropType

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './Button.scss';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 const Button = ({variant = '', ...otherProps}) => (
   <button 
     {...otherProps} 
     className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}
   />
 );
+
+Button.propTypes = {
+  variant: PropTypes.string,
+};
 
 export default Button;
