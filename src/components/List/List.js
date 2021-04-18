70d35aa701e3f2586d45ba812115ea064b87d0dc
naz <naz@o2.pl>
2021-04-18 12:12:01 +0200

    Add redux Column rendering

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+import Column from '../Column/ColumnContainer.js';
 //import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
