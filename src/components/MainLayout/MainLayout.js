6e4f481d72b443b95a3f54dae248320d57c6cc16
naz <naz@o2.pl>
2021-04-18 23:56:18 +0200

    Add MainLayout component

---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MainLayout = ({ children }) => <div>{children}</div>;
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default MainLayout;
