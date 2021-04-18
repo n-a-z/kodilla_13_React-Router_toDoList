8e185f051b1a3bb7d44189b6841dc83d5cbed884
naz <naz@o2.pl>
2021-04-18 23:59:19 +0200

    Add MainLayout to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
+import MainLayout from '../MainLayout/MainLayout';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
-      </Switch>
-    </BrowserRouter>
+    <MainLayout>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/info' component={Info} />
+        </Switch>
+      </BrowserRouter>
+    </MainLayout>
   </div>
 );
 
