c1402f8f0a506c48a19468cb421ec6f1e4bb00c9
naz <naz@o2.pl>
2021-04-18 23:54:49 +0200

    Add Router in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </BrowserRouter>
   </div>
 );
 
