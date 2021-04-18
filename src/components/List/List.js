bf29b7f767f97933200945d72e6bce62f0340915
naz <naz@o2.pl>
2021-04-18 10:05:57 +0200

    Add redux Column to List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-//import Column from '../Column/Column.js';
+import Column from '../Column/Column.js';
 //import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
@@ -20,19 +20,17 @@ class List extends React.Component {
   };
 
   render() {
-    const { title, image, description } = this.props;
+    const { title, image, description, columns } = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} imageUrl={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        {/*
+        <div className={styles.description}>{ReactHtmlParser(description)}</div>
         <div className={styles.columns}>
-          {this.state.columns.map(({ key, ...columnProps }) => (
-            <Column key={key} {...columnProps} />
+          {columns.map((columnData) => (
+            <Column key={columnData.id} {...columnData} />
           ))}
         </div>
+        {/*
         <div className={styles.creator}>
           <Creator
             text={settings.columnCreatorText}
