2cf984f6afde16dbbc09f6f529a04fd72569180b
naz <naz@o2.pl>
2021-04-19 11:44:16 +0200

    Add SearchResults component

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../Column/Column.scss';
+import { settings } from '../../data/dataStore';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
+
+class SearchResults extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    icon: PropTypes.string,
+    cards: PropTypes.array,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  };
+
+  render() {
+    const { title, icon, cards } = this.props;
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={icon} />
+          </span>
+          {title}
+        </h3>
+        <div>
+          {cards.map(({ key, ...cardProps }) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SearchResults;
